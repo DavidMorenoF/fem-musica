99774d6edc6c89538ee4b7fb013ad75500dbc6c6
David Moreno Fernández <144929862+DavidMorenoF@users.noreply.github.com>
2023-11-19 15:26:26 +0100

    setTempo

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
-    if (input.isGesture(Gesture.TiltLeft)) {
-        music.setTempo(90)
-    }
-    if (input.isGesture(Gesture.TiltRight)) {
-        music.setTempo(150)
-    }
+    music.play(music.stringPlayable("C5 B A G F E D C ", 90), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Ghost)
+    music.setTempo(160)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+    music.setTempo(40)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.StickFigure)
+    music.setTempo(60)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     music.play(music.stringPlayable("C5 G A F E G A G ", 120), music.PlaybackMode.UntilDone)
-    if (input.isGesture(Gesture.TiltLeft)) {
-        music.setTempo(90)
-    }
-    if (input.isGesture(Gesture.TiltRight)) {
-        music.setTempo(150)
-    }
 })
